72e703bff00d38df0a29e995f4606e03ea5d0c8b
Tim Van Driel <timothy.vandriel@gmail.com>
2024-11-12 12:49:22 -0700

    add comment to django settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party apps
     "debug_toolbar",
     # local apps
     "recipes",
