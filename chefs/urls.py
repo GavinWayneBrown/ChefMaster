835ded568a92c34bac61cf2793d2ff4a17fe5fd5
Gavin <gavinwaynebrown@gmail.com>
2024-12-07 22:42:46 -0700

    Set up GitHub Pages

--- chefs/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path 
-from .views import SignUpView, edit_bio
-urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("edit_bio/", edit_bio, name="edit_bio"),
-]
